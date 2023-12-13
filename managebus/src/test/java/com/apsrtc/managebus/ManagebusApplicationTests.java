a231bf1e937c705bb73a29739b430aeb180398c9
Vidhya-Mani1 <anandha.vidhya1@go-yubi.com>
2023-12-13 10:47:00 +0530

    initial commit check for project

---
+++ managebus/src/test/java/com/apsrtc/managebus/ManagebusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apsrtc.managebus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagebusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
